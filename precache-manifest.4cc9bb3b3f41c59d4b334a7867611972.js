da67badd5b1935321bd8dd23a1c0f62d0016d775
GamerKingFaiz <gamerkingfaiz@gmail.com>
2019-06-16 22:00:07 -0700

    Updates

---
+++ precache-manifest.4cc9bb3b3f41c59d4b334a7867611972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d40ef53547e0013533ab80a53fd753",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "bd04b532acbacc83a9c1",
+    "url": "/face-recognition-brain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "cfe004520f18a9685124",
+    "url": "/face-recognition-brain/static/css/main.8a7bf92d.chunk.css"
+  },
+  {
+    "revision": "bd04b532acbacc83a9c1",
+    "url": "/face-recognition-brain/static/js/2.19322cef.chunk.js"
+  },
+  {
+    "revision": "cfe004520f18a9685124",
+    "url": "/face-recognition-brain/static/js/main.02726e03.chunk.js"
+  },
+  {
+    "revision": "6e37b30b4e95597c3d6b",
+    "url": "/face-recognition-brain/static/js/runtime~main.6459330c.js"
+  }
+]);
